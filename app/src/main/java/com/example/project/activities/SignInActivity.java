879d1208441ffbeb95476581988af10698d8175c
oussama <abz1999abz@gmail.com>
2022-01-07 16:33:37 +0100

    rooting v1

--- app/src/main/java/com/example/project/activities/SignInActivity.java
+++ app/src/main/java/com/example/project/activities/SignInActivity.java
@@ -2,15 +2,26 @@ package com.example.project.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.project.R;
+
+import com.example.project.databinding.ActivitySignInBinding;
+
 
 public class SignInActivity extends AppCompatActivity {
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_in);
+        this.binding = ActivitySignInBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        this.binding.textViewCreateAccount.setOnClickListener(e ->
+                    startActivity(new Intent(getApplicationContext(),SignUpActivity.class)));
     }
 }
--- app/src/main/java/com/example/project/activities/SignUpActivity.java
+++ app/src/main/java/com/example/project/activities/SignUpActivity.java
@@ -2,15 +2,25 @@ package com.example.project.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.example.project.R;
+import com.example.project.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+    private ActivitySignUpBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        this.binding = ActivitySignUpBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        setListeners();
+    }
+
+    private void setListeners(){
+        this.binding.textViewToSignIn.setOnClickListener(e ->
+                startActivity(new Intent(getApplicationContext(),SignInActivity.class)));
     }
 }
